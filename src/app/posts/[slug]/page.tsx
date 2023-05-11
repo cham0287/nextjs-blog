ef2d0085fcd0b118c5c48a47dd2119ecf3e3c8ed
cham0287 <crtmt97@gmail.com>
2023-05-12 00:03:33 +0900

    Style: PostDetailPage 스타일 작업

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import MarkdownViewer from '@/components/posts/MarkdownViewer';
 import { getPostData } from '@/service/posts';
+import Image from 'next/image';
 import React from 'react';
+import { AiTwotoneCalendar } from 'react-icons/ai';
 
 interface Props {
   params: {
@@ -9,13 +11,27 @@ interface Props {
 }
 
 const PostDetail = async ({ params: { slug } }: Props) => {
-  const post = await getPostData(slug);
-  const { title, content } = post;
+  const { title, description, date, path, content } = await getPostData(slug);
   return (
-    <div>
-      <h2 className='text-2xl font-bold'>{title}</h2>
-      <MarkdownViewer content={content} />
-    </div>
+    <article className='rounded-2xl overflow-hidden bg-gray-100 shadow m-4 '>
+      <Image
+        className='w-full md:w-3/4 mx-auto md:mt-10 rounded-2xl'
+        src={`/images/posts/${path}.png`}
+        alt={title}
+        width={760}
+        height={420}
+      />
+      <section className='flex flex-col p-4'>
+        <div className='flex self-end items-center text-sky-600 gap-1 font-semibold'>
+          <AiTwotoneCalendar />
+          <p>{date.toString()}</p>
+        </div>
+        <h1 className='text-4xl font-bold'>{title}</h1>
+        <p className='text-xl font-bold'>{description}</p>
+        <div className='border-b-4 border-gray-300 my-5' />
+        <MarkdownViewer content={content} />
+      </section>
+    </article>
   );
 };
 
