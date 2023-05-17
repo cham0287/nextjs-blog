e5fa303e42b81de91f168d0f90093aa286e09b22
cham0287 <crtmt97@gmail.com>
2023-05-18 08:51:00 +0900

    Style(about): 클래스네임 상수로 수정ㅡ my-4 추가

--- src/components/about/Description.tsx
+++ src/components/about/Description.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
+const TITLE_CLASS = 'text-2xl font-bold my-2';
+
 const Description = () => {
   return (
     <div className='bg-gray-100 w-3/4 mx-auto my-6 p-4 text-center shadow-md'>
-      <h2 className='text-2xl font-bold'>Who Am I?</h2>
+      <h2 className={TITLE_CLASS}>Who Am I?</h2>
       <p>Junior Front-end Developer</p>
-      <h2 className='text-2xl font-bold'>Career</h2>
+      <h2 className={TITLE_CLASS}>Career</h2>
       <p>krossgames (2023.03 ~ Now)</p>
-      <h2 className='text-2xl font-bold'>Skills</h2>
+      <h2 className={TITLE_CLASS}>Skills</h2>
       <p>Javascript, React, Typescript, Next, Node</p>
       <p>Recoil, SWR, React Query</p>
     </div>
